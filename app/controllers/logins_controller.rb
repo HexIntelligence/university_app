eb368e090f56e2e24923544c4044760a00941c0c
Ebenezer Ankrah <ebenezer.ankrah@weknowtraining.ca>
2023-05-08 19:24:19 -0400

    Authentication system - build routes and form

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,17 @@
+class LoginsController < ApplicationController
+  
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+
+  def destroy
+    redirect_to login_path
+  end 
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # get 'students', to: 'students#new'
   resources :students, except: [:destroy]
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
 end
